906731f04bf18834cbf573d22941b10566152799
Beforejamn1 <beforejamn1@gmail.com>
2026-06-04 10:38:07 +0900

    데이터 타입2

---
+++ 04.js
@@ -0,0 +1,33 @@
+//Boolean
+//true, false
+let flagT = true;
+let flagF = false;
+
+console.log(flagT);
+console.log(typeof flagT);
+console.log(flagF);
+console.log(typeof flagF);
+
+// undefined : 할당되지 않음 
+let un;
+console.log(un);
+console.log(typeof un);
+
+//null : 값이 존재하지 않음을 '명시적'으로 나타내는 방법
+//null != undefined
+let nu = null;
+console.log(nu);
+console.log(typeof nu);
+
+//object : key-value
+let person = {
+    name: "choi",
+    age: 20,
+    isMarried: true
+}
+console.log(person);
+console.log(typeof person);
+
+//array
+let number = [1, 2, 3, 4, 5];
+let fruit = ['apple', 'banana', 'orange'];
